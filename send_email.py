baaa71d72e22d9eaf58ea24294b6741775843c65
Sravan Kumar <sravan0783@gmail.com>
2024-04-16 07:49:32 +0530

    first commit

---
+++ send_email.py
@@ -0,0 +1,36 @@
+import smtplib
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# Set up the SMTP server details
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587  # TLS port
+sender_email = os.environ['SENDER_EMAIL']
+sender_password = os.environ['SENDER_EMAIL_PASSWORD']
+
+# Create a message
+message = MIMEMultipart()
+message['From'] = sender_email
+message['To'] = os.environ['RECEIPIENTS']
+message['Subject'] = os.environ['EMAIL_SUBJECT']
+
+# Add body to email
+body = os.environ['EMAIL_BODY']
+message.attach(MIMEText(body, 'plain'))
+
+# Connect to the SMTP server
+server = smtplib.SMTP(smtp_server, smtp_port)
+server.starttls()
+
+# Login to Gmail
+server.login(sender_email, sender_password)
+
+# Send the email
+for _ in range(10):
+    server.send_message(message)
+
+# Quit the server
+server.quit()
+
+print("Email sent successfully!")
